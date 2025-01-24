27cee7b1851989d93c5707e47a430ec8219bbbec
June <df41022@gmail.com>
2025-01-24 20:22:24 +0800

    ticket.sol

---
+++ contract/ticket.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TicketNFT is ERC721, Ownable(msg.sender) {
+    uint256 public nextTokenId;
+    mapping(uint256 => bool) public validTickets;
+
+    constructor() ERC721("TicketNFT", "TNFT") {}
+
+    function buyTicket() external {
+        uint256 tokenId = nextTokenId;
+        _safeMint(msg.sender, tokenId);
+        validTickets[tokenId] = true;
+        nextTokenId++;
+    }
+
+    function cancelTicket(uint256 tokenId) external {
+        require(
+            ownerOf(tokenId) == msg.sender,
+            "You are not the owner of this ticket"
+        );
+        require(validTickets[tokenId], "Ticket is already invalid");
+        validTickets[tokenId] = false;
+    }
+
+    function isTicketValid(uint256 tokenId) external view returns (bool) {
+        return validTickets[tokenId];
+    }
+}
